c58fd04bac2ac0b27fc0939e75407852ae5f2e59
Sujaur312998 <suja312998@gmail.com>
2024-07-16 02:05:37 +0600

    Registration Function added

--- actions/register.ts
+++ actions/register.ts
@@ -1,13 +1,31 @@
 "use server";
 import * as z from "zod"
 import { RegistrationSchema } from "@/schemas";
-
+import bcrypt from 'bcrypt'
+import {db} from '@/lib/db'
+import { getUserByEmail } from "@/data/user";
 
 export const register = async ( values: z.infer<typeof RegistrationSchema> ) => {
     const validatedFields= RegistrationSchema.safeParse(values)
     if(!validatedFields.success){
         return {error: 'Invalid Fields!'}
     }
+    const {email, password, name}= validatedFields.data
+    
+    const existingUser= await getUserByEmail(email)
 
-    return { success: "Email Sent !"}
+    if(existingUser) return {error: "Email already in use!"}
+    const hashedPassword=  await bcrypt.hash(password,10)
+
+    await db.user.create({
+        data:{
+            name,
+            email,
+            password:hashedPassword
+        }
+    })
+
+    //TODO SENT VERIFICATION EMAIL
+
+    return { success: "User Created!"}
 }
